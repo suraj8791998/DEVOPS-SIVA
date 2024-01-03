e42fff3f8b71a2656765cb29b8431be3c555ec92
Suraj kumar <mulaveesalav@gmail.com>
2024-01-03 20:32:53 +0530

    Variable first

---
+++ 03-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# IT HELPS TO EXECUTE A COMMAND INSIDE SHELL SCRIPT AND TAKE THE OUTPUT
+
+DATE=$(date)
+
+echo " This is suraj"
+
+echo "I was learning Devops"
+
+echo "Today DATE was :$DATE"
+
