17f5f24eb6b0c62e256d8767921ea0f758c79596
Suraj kumar <mulaveesalav@gmail.com>
2024-01-04 17:48:34 +0530

    Variable first

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -6,10 +6,10 @@ if [ $NUMBER -gt 10 ]
 
 then
    
-   echo "Number is smaller than 10"
+   echo "Number is greater than 10"
 
 else
 
-   echo "Number is greater than 10"
+   echo "Number is smaller than 10"
 
 fi
