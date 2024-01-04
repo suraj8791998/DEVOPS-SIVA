cc5b3c9c56438c805fb750c6742ca63c0d3d075c
Suraj kumar <mulaveesalav@gmail.com>
2024-01-04 18:04:54 +0530

    Variable first

---
+++ 09-assignment1.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+DATE=$(date)
+
+USERID=$(id -u) 
+
+#CHECKING IF THE USER IN ROOT OR NOT
+if [ $USERID -ne 0 ]
+then
+    echo "Please switch to root user"
+else
+    echo "you are in root"
+fi
+
+# CHECKING WHETHER PACKAGE IS AVAILBLE OR NOT
+if [ rpm -q maven ]
+then
+    echo "Mavane package is availble"
+    exit 0
+else
+    echo "Package is not availble, Installe the package"
+    yum install maven -y
+
+fi
+
+if [ rpm -q git ]
+then
+    echo "Mavane package is availble"
+    exit 0
+else
+    echo "Package is not availble, Installe the package"
+    yum install git -y
+
+fi
