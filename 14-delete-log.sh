6272be4263b9dc4a6d4beb21a8698e0ff49efa1c
Suraj kumar <mulaveesalav@gmail.com>
2024-01-09 11:11:25 +0530

    updated files

---
+++ 14-delete-log.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+LOG_FOLDER=/home/centos/shell-scirpt-log
+
+DATE=$(date +%F)
+LOGDIR=/home/centos/deleted-log
+LOG_STORE=$LOGDIR-$DATE
+
+FILES_TO_DELETE=$(find $LOG_FOLDER -name "*.log")
