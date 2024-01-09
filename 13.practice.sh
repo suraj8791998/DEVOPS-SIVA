f6a0045e8b738c0a380c50f0d679e0776377a7bc
Suraj kumar <mulaveesalav@gmail.com>
2024-01-09 10:16:20 +0530

    updated files

--- 13.practice.sh
+++ 13.practice.sh
@@ -2,36 +2,31 @@
 
 USERID=$(id -u)
 
+R="/e[31m"
+N="/e[0m"
+G="/e[32m"
+
+
 #CHECKING USER IS ON ROOT OR NOT
 
 if [ $USERID -ne 0 ]
 
 then 
 
-   echo "ERROR:: PLEASE SWITCH TO RROT USER"
+   echo " $R ERROR:: PLEASE SWITCH TO RROT USER $N"
 
    exit 1
 fi
 
 #CHECKING WHETHER PACKAGE IS INSTALLED OR NOT
-# VALIDATE() {
-
-#     if [ $? -ne 0 ]
-
-#     then
-#        echo "INSTALLING $2 IS FAILURE"
-
-#     else
-#        echo "INSTALLING $2 IS SUCCESS"
-# }
 
 VALIDATE(){
     if [ $1 -ne 0 ];
     then
-        echo "INSTALLING $2 IS FAILURE"
+        echo "INSTALLING $R $2 IS FAILURE $N"
         exit 1
     else
-         echo "INSTALLING $2 IS SUCCESS"
+         echo "INSTALLING $G $2 IS SUCCESS $N"
     fi
 }
 #INSTALLING PACVKAGES AND CHECKING WHETHER PACKAGE IS ALREADY AVAILABLE
@@ -44,13 +39,13 @@ do
 
   then
     
-      echo "PACKAGE IS NOT YET INSTALLED"
+      echo "$R PACKAGE IS NOT YET INSTALLED $N"
       yum install $i -y
       VALIDATE $? $i
 
   else
 
-      echo "PACKAGE IS ALREADY INSTALLED"
+      echo "$G PACKAGE IS ALREADY INSTALLED $N"
     
   fi
 done
