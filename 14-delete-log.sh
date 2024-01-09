4079ea8620e4b83ac981d1ff2644c52aeb0381f1
Suraj kumar <mulaveesalav@gmail.com>
2024-01-09 11:17:56 +0530

    updated files

--- 14-delete-log.sh
+++ 14-delete-log.sh
@@ -9,3 +9,13 @@ LOG_STORE=$LOGDIR-$DATE
 FILES_TO_DELETE=$(find $LOG_FOLDER -name "*.log")
 
 echo $FILES_TO_DELETE
+
+while read line
+
+do
+
+    echo "Deleting $line" &>>$LOG_STORE
+
+    rm -rf $line
+
+done <<< $FILES_TO_DELETE
